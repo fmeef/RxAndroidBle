db4f0b07ad4adc05074a854287b613a8cb17a8f2
Alex Ballmer <gnu3ra@riseup.net>
2020-07-12 21:25:38 +0000

    Cleanup
    - remove unused dagger provides in ServerComponent
    - remote misguided RxBleComponent interface

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/ClientComponent.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/ClientComponent.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Build;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
 
@@ -55,7 +54,56 @@ import io.reactivex.schedulers.Schedulers;
 
 @ClientScope
 @Component(modules = {ClientComponent.ClientModule.class})
-public interface ClientComponent extends RxBleComponent {
+public interface ClientComponent {
+
+    class NamedExecutors {
+
+        public static final String BLUETOOTH_INTERACTION = "executor_bluetooth_interaction";
+        public static final String CONNECTION_QUEUE = "executor_connection_queue";
+        private NamedExecutors() {
+
+        }
+    }
+
+    class NamedSchedulers {
+
+        public static final String COMPUTATION = "computation";
+        public static final String TIMEOUT = "timeout";
+        public static final String BLUETOOTH_INTERACTION = "bluetooth_interaction";
+        public static final String BLUETOOTH_CALLBACKS = "bluetooth_callbacks";
+        private NamedSchedulers() {
+
+        }
+    }
+
+    class PlatformConstants {
+
+        public static final String INT_TARGET_SDK = "target-sdk";
+        public static final String INT_DEVICE_SDK = "device-sdk";
+        public static final String BOOL_IS_ANDROID_WEAR = "android-wear";
+        public static final String STRING_ARRAY_SCAN_PERMISSIONS = "scan-permissions";
+        private PlatformConstants() {
+
+        }
+    }
+
+    class NamedBooleanObservables {
+
+        public static final String LOCATION_SERVICES_OK = "location-ok-boolean-observable";
+        private NamedBooleanObservables() {
+
+        }
+    }
+
+    class BluetoothConstants {
+
+        public static final String ENABLE_NOTIFICATION_VALUE = "enable-notification-value";
+        public static final String ENABLE_INDICATION_VALUE = "enable-indication-value";
+        public static final String DISABLE_NOTIFICATION_VALUE = "disable-notification-value";
+        private BluetoothConstants() {
+
+        }
+    }
 
     @Component.Builder
     interface Builder {
@@ -276,9 +324,9 @@ public interface ClientComponent extends RxBleComponent {
         abstract Function<RxBleInternalScanResult, ScanResult> provideScanResultMapper(InternalToExternalScanResultConverter mapper);
     }
 
-    RxBleClient rxBleClient();
-
     LocationServicesOkObservable locationServicesOkObservable();
+
+    RxBleClient rxBleClient();
 
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     interface ClientComponentFinalizer {
