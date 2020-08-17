f79d503a10680ad7707eb9419321b7324e3e65c5
Alex Ballmer <gnu3ra@riseup.net>
2020-08-16 22:33:58 -0500

    simplistic interface to add services and set phy

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/ServerConnector.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/connection/ServerConnector.java
@@ -1,9 +1,11 @@
 package com.polidea.rxandroidble2.internal.connection;
 
+import com.polidea.rxandroidble2.ServerConfig;
 import com.polidea.rxandroidble2.internal.server.RxBleServerConnection;
 
 import io.reactivex.Observable;
 
 public interface ServerConnector {
-    Observable<RxBleServerConnection> subscribeToConnections();
+    Observable<RxBleServerConnection> subscribeToConnections(ServerConfig config);
+    void closeServer();
 }
