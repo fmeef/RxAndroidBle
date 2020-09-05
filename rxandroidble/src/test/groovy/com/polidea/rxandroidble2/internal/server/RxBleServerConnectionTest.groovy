ce65a2c20524266d96cc24e54ca1cf5c07b25e8f
Alex Ballmer <gnu3ra@riseup.net>
2020-09-05 18:35:42 -0500

    RxBleServerConnection unit test
    - fix a number of logic issues with characteristic / descriptor long write handling

--- rxandroidble/src/test/groovy/com/polidea/rxandroidble2/internal/server/RxBleServerConnectionTest.groovy
+++ rxandroidble/src/test/groovy/com/polidea/rxandroidble2/internal/server/RxBleServerConnectionTest.groovy
@@ -2,18 +2,15 @@ package com.polidea.rxandroidble2.internal.server
 
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
 import android.bluetooth.BluetoothGattServer
-import com.jakewharton.rxrelay2.PublishRelay
 import com.polidea.rxandroidble2.DummyOperationQueue
 import com.polidea.rxandroidble2.internal.operations.server.ServerOperationsProvider
 import com.polidea.rxandroidble2.internal.operations.server.ServerOperationsProviderImpl
 import com.polidea.rxandroidble2.internal.serialization.QueueReleaseInterface
-import io.reactivex.Observable
-import io.reactivex.Scheduler
 import io.reactivex.annotations.NonNull
 import io.reactivex.functions.Predicate
 import io.reactivex.schedulers.TestScheduler
-import io.reactivex.subjects.PublishSubject
 import spock.lang.Specification
 
 import java.util.concurrent.TimeUnit
@@ -34,15 +31,19 @@ public class RxBleServerConnectionTest extends Specification {
             BluetoothGattCharacteristic.PROPERTY_READ,
             BluetoothGattCharacteristic.PERMISSION_READ
     )
+
+    BluetoothGattDescriptor descriptor = new BluetoothGattDescriptor(
+            testUuid,
+            BluetoothGattDescriptor.PERMISSION_READ
+    )
+
     int requestID  = 1
     byte[] data = [1,2,3,4,5,6,7,8,9,0]
     long repeat = 10
     byte[] finaldata = new byte[data.length * repeat]
-    Observable<byte[]> inputBytesObservable
 
 
     def setup() {
-        inputBytesObservable = Observable.just(data).repeat(repeat)
         for (int i=0;i<finaldata.length;i++) {
             finaldata[i] = data[i % data.length]
         }
@@ -61,16 +62,17 @@ public class RxBleServerConnectionTest extends Specification {
     }
 
 
-    def "longWriteOutput works"() {
+    def "longWriteOutput emits correct value"() {
         when:
-        RxBleServerConnection.Output<byte[]> output = objectUnderTest.openLongWriteOutput(requestID, characteristic)
-        PublishSubject<byte[]> o = output.valueRelay
-        o.onNext(finaldata)
-        advanceTimeForWritesToComplete(1)
+        prepCharacteristicLongWriteOutput()
+        def closeresult = objectUnderTest.closeLongWriteCharacteristicOutput(requestID).test()
+        advanceTimeForWrites(1)
 
         then:
-        def testsubscriber = objectUnderTest.getLongWriteObservable(requestID).test()
-        testsubscriber.assertValue(new Predicate<byte[]>() {
+        closeresult.assertNoErrors()
+        closeresult
+                .assertNoErrors()
+                .assertValue(new Predicate<byte[]>() {
             @Override
             boolean test(@NonNull byte[] bytes) throws Exception {
                 return Arrays.equals(bytes, finaldata)
@@ -78,6 +80,68 @@ public class RxBleServerConnectionTest extends Specification {
         })
 
     }
+
+    def "longWriteOutput handles descriptor write successfully"() {
+        when:
+        prepDescriptorLongWriteOutput()
+        def closeresult = objectUnderTest.closeLongWriteDescriptorOutput(requestID).test()
+        advanceTimeForWrites(1)
+
+        then:
+        closeresult.assertNoErrors()
+        closeresult
+                .assertNoErrors()
+                .assertValue(new Predicate<byte[]>() {
+                    @Override
+                    boolean test(@NonNull byte[] bytes) throws Exception {
+                        return Arrays.equals(bytes, finaldata)
+                    }
+                })
+    }
+
+
+    def "characteristicLongWriteOutput handles nonexistent requestid"() {
+        when:
+        prepCharacteristicLongWriteOutput()
+        def closeresult = objectUnderTest.closeLongWriteCharacteristicOutput(0).test()
+        advanceTimeForWrites(1)
+
+        then:
+        closeresult.assertNoErrors()
+        closeresult.assertEmpty()
+
+    }
+
+    def "descriptorLongWriteOutput handles nonexistent requestid"() {
+        when:
+        prepDescriptorLongWriteOutput()
+        def closeresult = objectUnderTest.closeLongWriteDescriptorOutput(0).test()
+        advanceTimeForWrites(1)
+
+        then:
+        closeresult.assertNoErrors()
+        closeresult.assertEmpty()
+
+    }
+
+    private prepCharacteristicLongWriteOutput() {
+        def output = objectUnderTest.openLongWriteCharacteristicOutput(requestID, characteristic)
+        advanceTimeForWrites(1)
+        for (int x=0;x<repeat;x++) {
+            output.valueRelay.onNext(data)
+            advanceTimeForWrites(1)
+        }
+    }
+
+    private prepDescriptorLongWriteOutput() {
+        def output = objectUnderTest.openLongWriteDescriptorOutput(requestID, descriptor)
+        advanceTimeForWrites(1)
+        for (int x=0;x<repeat;x++) {
+            output.valueRelay.onNext(data)
+            advanceTimeForWrites(1)
+        }
+    }
+
     private advanceTimeForWrites(long numberOfWrites) {
         testScheduler.advanceTimeBy(numberOfWrites * DEFAULT_WRITE_DELAY, TimeUnit.SECONDS)
     }
