69b00e90ee8e2928901c3a751bd965f69e552687
Alex Ballmer <gnu3ra@riseup.net>
2020-09-12 14:50:48 -0500

    add ServerDisconnectOperation

--- rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/operations/server/ServerConnectionOperationsProviderImpl.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble2/internal/operations/server/ServerConnectionOperationsProviderImpl.java
@@ -3,25 +3,34 @@ package com.polidea.rxandroidble2.internal.operations.server;
 import android.bluetooth.BluetoothDevice;
 
 import com.polidea.rxandroidble2.ServerComponent;
+import com.polidea.rxandroidble2.ServerConnectionScope;
 import com.polidea.rxandroidble2.internal.server.BluetoothGattServerProvider;
+import com.polidea.rxandroidble2.internal.server.RxBleGattServerCallback;
 
 import bleshadow.javax.inject.Inject;
 import bleshadow.javax.inject.Named;
 import io.reactivex.Scheduler;
 
+@ServerConnectionScope
 public class ServerConnectionOperationsProviderImpl implements ServerConnectionOperationsProvider {
 
     private final BluetoothGattServerProvider bluetoothGattServer;
     private final Scheduler gattServerScheduler;
+    private final BluetoothDevice bluetoothDevice;
+    private final RxBleGattServerCallback callback;
 
 
     @Inject
     public ServerConnectionOperationsProviderImpl(
             @Named(ServerComponent.NamedSchedulers.BLUETOOTH_INTERACTION) Scheduler gattServerScheduler,
-            BluetoothGattServerProvider bluetoothGattServer
+            BluetoothGattServerProvider bluetoothGattServer,
+            BluetoothDevice bluetoothDevice,
+            RxBleGattServerCallback callback
     ) {
         this.gattServerScheduler = gattServerScheduler;
         this.bluetoothGattServer = bluetoothGattServer;
+        this.bluetoothDevice = bluetoothDevice;
+        this.callback = callback;
     }
 
 
@@ -43,5 +52,14 @@ public class ServerConnectionOperationsProviderImpl implements ServerConnectionO
                 offset,
                 value
         );
+    }
+
+    public ServerDisconnectOperation provideDisconnectOperation(BluetoothDevice device) {
+        return new ServerDisconnectOperation(
+                bluetoothGattServer,
+                device,
+                callback,
+                gattServerScheduler
+                );
     }
 }
