9b509875b0d5d6c78e937212d25ee669f7dcddde
Alex Ballmer <gnu3ra@riseup.net>
2020-09-27 01:53:17 -0500

    Builder for RxBleServerConnectionMock

--- mockrxandroidble/src/main/java/com/polidea/rxandroidble2/mockrxandroidble/RxBleServerMock.java
+++ mockrxandroidble/src/main/java/com/polidea/rxandroidble2/mockrxandroidble/RxBleServerMock.java
@@ -5,9 +5,18 @@ import android.bluetooth.BluetoothDevice;
 import com.polidea.rxandroidble2.RxBleServer;
 import com.polidea.rxandroidble2.RxBleServerConnection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.reactivex.Observable;
+import io.reactivex.subjects.BehaviorSubject;
 
 public class RxBleServerMock extends RxBleServer {
+
+    private final BehaviorSubject<BluetoothDevice> connectedDevices = BehaviorSubject.create();
+    private final Map<BluetoothDevice, RxBleServerConnectionMock> connectionMap = new HashMap<>();
+
+
     //TODO:
     @Override
     public Observable<State> observeStateChanges() {
